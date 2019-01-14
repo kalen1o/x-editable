bedb3cc8c1d997fbf4ef18e6ff91980c300d42d9
KALEN <ngungonngon@gmail.com>
2019-01-14 21:37:38 +0200

    classwork

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$('body').on('click', '[editable]', function(event) {
+	event.preventDefault(); //отмена дефолтного действия
+
+	var type = $(this).attr('editable'),
+		$element = $(this);
+
+	$('<input>')
+		.insertAfter($element) //создаем после
+		.attr('type', type) 
+		.val($element.text()) //даем форме значение
+		.focus() //сразу фокусирует мышку на форме
+		.select() //сразу выбирает все значение формы
+		.on('keyup', function(event) {
+			if(event.which === 13) { //Enter
+				$element
+					.insertAfter(this)
+					.text( $(this).val() );
+
+				$(this).remove();
+			} else if(event.which === 27) { //ESC
+				$(this).trigger('blur'); //вызывает BLUR
+			}	
+		})
+		.on('blur', function() {
+			$element.insertAfter(this);
+
+			$(this).remove(); //Удаляет
+		});
+
+	$element.remove();
+});
