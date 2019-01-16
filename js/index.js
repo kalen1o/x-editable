2d0ce557ccc55c062f08da56db54a81b22557bb5
KALEN <ngungonngon@gmail.com>
2019-01-16 23:37:59 +0200

    100% x-editable

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $('body').on('click', '[editable]', function(event) {
 	var type = $(this).attr('editable'),
 		$element = $(this);
 
-	$('<input>')
+	var $input = $('<input>')
 		.insertAfter($element) //создаем после
 		.attr('type', type) 
 		.val($element.text()) //даем форме значение
@@ -12,23 +12,48 @@ $('body').on('click', '[editable]', function(event) {
 		.select() //сразу выбирает все значение формы
 		.on('keyup', function(event) {
 			if(event.which === 13) { //Enter
-				$element
-					.insertAfter(this)
-					.text( $(this).val() );
-
-				$(this).remove();
+				$ok.trigger('click');
 			} else if(event.which === 27) { //ESC
-				$(this).trigger('blur'); //вызывает BLUR
+				//$(this).trigger('blur'); //вызывает BLUR
+				$cancel.trigger('click');
 			}	
-		})
-		.on('blur', function() {
-			$element.insertAfter(this);
-
-			$(this).remove(); //Удаляет
 		});
 
-	$element.remove();
-});
+	var $ok = $('<button>')
+					.insertAfter($input)
+					.attr('class', 'btn btn-primary')
+					.on('click', function(event) {
+						$element
+							.insertAfter(this)
+							.text( $input.val() );
+						$input.remove();
+						$cancel.remove();
+						$(this).remove();
+					});
 
-console.log('world')
-console.log('hello');
+	$('<span>')
+		.appendTo($ok)
+		.attr({'class': 'glyphicon glyphicon-ok', 'aria-hidden': 'true'});
+
+	var $cancel = $('<button>')
+					.insertAfter($ok)
+					.attr('class', 'btn btn-default')
+					.on('click', function(event) {
+						$element.insertAfter(this);
+						$input.remove();
+						$ok.remove();
+						$(this).remove();
+					});
+
+	$('<span>')
+		.appendTo($cancel)
+		.attr({'class': 'glyphicon glyphicon-remove', 'aria-hidden': 'true'});
+
+	$('body').on('mousedown', function(event) {
+		console.log('mousedown')
+		$cancel.trigger('click');
+		$(this).off('mousedown');
+	})
+	
+		$element.remove();
+});
